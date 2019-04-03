507bfa9ad077146e82ba30689a9ada2ac01e86c5
wallace jesus <wallace@topdown.com.br>
2019-04-03 09:41:59 -0300

    retirando acesso ao bd

--- api/core/dao/Dao.php
+++ api/core/dao/Dao.php
@@ -4,7 +4,7 @@ class Dao{
   public function open(){
   	$conn = null;
   	try {
-  		$conn =  new MongoDB\Client('mongodb+srv://api_user:TOiaxMkhvJa9QPdY@cluster0-bw26h.mongodb.net/test?retryWrites=true');     	} 
+  		$conn =  new MongoDB\Client('mongodb+srv://user:password@host');     	} 
   	catch (Exception $e) {
   	  $conn=null;
   	  throw new Exception("Erro ao tentar conectar com o banco de dados.", 1);
